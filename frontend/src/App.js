8e34e2274fecb89aa4744ffc11be3f6e2132687d
Sreya Karmakar <sreyaworldz@gmail.com>
2024-09-06 02:13:32 +0530

    adding frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Container>
+      <main className="py-3">
+         <HomeScreen />
+        </main>
+      </Container>
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
