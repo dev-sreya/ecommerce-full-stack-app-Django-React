fa5457d4b1e43181dd9bf158c16cb1489fa70b14
Sreya Karmakar <sreyaworldz@gmail.com>
2024-10-09 04:29:37 +0530

    new files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'media'
+] 
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'media/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
-
+from .models import Product
+ 
 
 # def getRoutes(request):
 #     return JsonResponse({'message': 'Hello'})
@@ -19,6 +20,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
+    products = Product,objecta.all()
     return Response(products)
 
 @api_view(['GET'])
