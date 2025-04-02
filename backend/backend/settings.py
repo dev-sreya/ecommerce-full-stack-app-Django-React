07d6434c279204eac17c6c7bba2aaff6e196e715
Sreya Karmakar <sreyaworldz@gmail.com>
2025-04-03 02:51:33 +0530

    Serializing user data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
@@ -80,7 +80,7 @@ SIMPLE_JWT = {
     "JTI_CLAIM": "jti",
 
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-    "SLIDING_TOKEN_LIFETIME": timedelta(days=30),
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 
     "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
@@ -92,11 +92,10 @@ SIMPLE_JWT = {
 }
 
 
-SIMPLE_JWT = {
-  # It will work instead of the default serializer(TokenObtainPairSerializer).
-  "TOKEN_OBTAIN_SERIALIZER": "my_app.serializers.MyTokenObtainPairSerializer",
-  # ...
-}
+# SIMPLE_JWT = {
+#   # It will work instead of the default serializer(TokenObtainPairSerializer).
+#   "TOKEN_OBTAIN_SERIALIZER": "backend.serializers.MyTokenObtainPairSerializer",
+# }
 
 
 MIDDLEWARE = [
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
 from .products import products
 from .models import Product
  
-from .serializers import ProductSerializer, UserSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -16,16 +17,21 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def validate(self, user):
-        data = super().validate(user)
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
 
-        # Add custom claims
-        data['username'] = self.user.username
-        data['email'] = self.user.email
-        # ...
+        # Add user details
+        serializer = UserSerializerWithToken(self.user).data
+        data.update(serializer)
+        # for k, v in serializer.items():
+        #     data[k] = v  
 
         return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 
 
@@ -39,6 +45,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many = False)
