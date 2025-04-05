452f589b877c47d9e255fdf18fb24509c1f39f25
Sreya Karmakar <sreyaworldz@gmail.com>
2025-04-05 12:35:24 +0530

    building feature: User can login with email and structuring the paths

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,14 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import sys
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
 
+
+import base.urls.product_urls as product_urls
+import base.urls.order_urls as order_urls
+import base.urls.user_urls as user_urls
+#sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)),'base'))
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
+   path('api/products/', include(product_urls.product_urrls)),
+    path('api/users/', include(user_urls.user_urrls)),
+    path('api/ordered/', include(order_urls.order_urrls)),
+    #path('api/', include('base.urls')),
     #path('api-auth/', include('rest_framework.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -4,21 +4,18 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
-from .products import products
-from .models import Product
+
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
-
  
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-# def getRoutes(request):
-#     return JsonResponse({'message': 'Hello'})
+
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -58,15 +55,6 @@ def registerUser(request):
 
 
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-
-    ]
-    return Response(routes)
-
-
-
-@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
@@ -79,28 +67,4 @@ def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many = True)
     return Response(serializer.data)
-
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many = True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request,pk):
-    # product = None
-    # for i in products:
-    #     if i['_id'] == pk:
-    #         product = i
-    #         break
-    try:
-        product = Product.objects.get(_id=pk)
-        serializer = ProductSerializer(product, many=False)
-        return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response({'detail': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
-        
 
