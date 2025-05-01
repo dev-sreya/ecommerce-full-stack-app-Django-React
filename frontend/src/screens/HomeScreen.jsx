4b26be83c4cbfb79514f0caa61390af4b87dba50
Sreya Karmakar <sreyaworldz@gmail.com>
2025-05-01 23:55:32 +0530

    Product Carousel

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,6 +6,7 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import ProductCarousel from '../components/ProductCarousel'
 import Paginate from '../components/Paginate'
 //import products from "../products";
  
@@ -16,9 +17,10 @@ function HomeScreen() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const location = useLocation()
-  const keyword = location.search
   const productList = useSelector(state => state.productList)
-    const { error, loading, products, page, pages } = productList
+  const { error, loading, products, page, pages } = productList
+
+  const keyword = location.search
  
   useEffect(() => {
     dispatch(listProducts(keyword))
@@ -27,22 +29,24 @@ function HomeScreen() {
 
   return (
     <div>
-      <h1>Latest products</h1>
-      {loading && <Loader />}
-      {error && <Message varient="danger">{error}</Message>}
- 
-      <div>
-          <Row>
-              {products.map(product => (
-                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                      <Product product={product} />
-                  </Col>
-              ))}
-          </Row>
-          <Paginate page={page} pages={pages} keyword={keyword} />
-      </div>
+        {!keyword && <ProductCarousel />}
+
+        <h1>Latest Products</h1>
+        {loading ? <Loader />
+            : error ? <Message variant='danger'>{error}</Message>
+                :
+                <div>
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <Paginate page={page} pages={pages} keyword={keyword} />
+                </div>
+        }
     </div>
-  );
+)
 }
- 
 export default HomeScreen;
